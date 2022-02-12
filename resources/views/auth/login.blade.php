5c9994d6552d0d006f2d72bf722b1bdcd0208f5a
Christian Leo-Pernold <christian.leo@akwien.at>
2022-02-12 15:37:08 +0100

    WIP.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,43 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html class="h-full bg-gray-50">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+<body class="h-full">
+    <div class="flex items-center justify-center min-h-full px-4 py-12 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <div>
+                <x-fas-money-bill-alt class="w-auto h-12 mx-auto text-gray-800" />
+            </div>
+            <form class="mt-8 space-y-6" action="{{ route('login') }}" method="POST">
+                @csrf
+                <input type="hidden" name="remember" value="true">
+                <div class="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-t-md focus:outline-none focus:ring-gray-800 focus:border-gray-800 focus:z-10 sm:text-sm" placeholder="Email address">
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full px-3 py-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-none appearance-none rounded-b-md focus:outline-none focus:ring-gray-800 focus:border-gray-800 focus:z-10 sm:text-sm" placeholder="Password">
+                    </div>
+                </div>
 
-@section('content')
-@include('layouts.errors')
-<form
-    action="{{ route('login') }}"
-    method="POST"
-    class="flex flex-col mx-auto justify-around h-32 mt-16"
->
-    {{ csrf_field() }}
-
-    <input
-        type="text"
-        name="email"
-        class="bg-gray-100 px-4 py-2 focus:bg-white"
-        placeholder="Username"
-        autofocus
-        required
-    >
-    <input
-        type="password"
-        name="password"
-        class="bg-gray-100 px-4 py-2 focus:bg-white"
-        placeholder="Password"
-        required
-    >
-    <button
-        type="submit"
-        class="bg-gray-300 border border-gray-600 px-4 py-2"
-    >Login</button>
-</form>
-@endsection
+                <div>
+                    <button type="submit" class="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-gray-600 border border-transparent rounded-md group hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800">
+                        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <x-heroicon-s-lock-closed class="w-5 h-5 text-gray-800 group-hover:text-gray-400" />
+                        </span>
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -10,45 +10,28 @@
         <x-heroicon-o-menu-alt-2 class="w-6 h-6" />
     </button>
 
-    <div class="flex justify-between flex-1 px-4">
-        <div class="flex flex-1">
-            <form class="flex w-full md:ml-0" action="#" method="GET">
-                <label for="search-field" class="sr-only">Search</label>
-                <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                        <x-heroicon-s-search class="w-5 h-5" />
-                    </div>
-
-                    <input id="search-field" class="block w-full h-full py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 border-transparent focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm" placeholder="Search" type="search" name="search">
-                </div>
-            </form>
-        </div>
-
-        <div class="flex items-center ml-4 md:ml-6">
-            <button type="button" class="p-1 text-gray-400 bg-white rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                <span class="sr-only">View notifications</span>
-                <!-- Heroicon name: outline/bell -->
-                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
-            </button>
-
+    <div class="flex justify-end flex-1 px-4 ">
+        <div
+            @click.away="showDropdown = false"
+            class="flex items-center ml-4 md:ml-6"
+        >
             <!-- Profile dropdown -->
             <div class="relative ml-3">
                 <div>
                     <button
-                        @click="showDropdown = true"
+                        @click="showDropdown = !showDropdown"
                         type="button" class="flex items-center max-w-xs text-sm bg-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         id="user-menu-button"
                         aria-expanded="false"
                         aria-haspopup="true"
                     >
                         <span class="sr-only">Open user menu</span>
-                        <img class="w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                        <div class="flex items-center justify-center w-8 h-8 text-white bg-gray-800 rounded-full">CL</div>
                     </button>
                 </div>
 
                 <div
+                    x-show="showDropdown"
                     class="absolute right-0 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                     role="menu"
                     aria-orientation="vertical"
@@ -63,9 +46,11 @@
                 >
                     <a
                         href="#"
+                        @click="$refs.logoutForm.submit()"
                         class="block px-4 py-2 text-sm text-gray-700" role="menuitem"
                         tabindex="-1"
                     >Sign out</a>
+                    <form action="{{ route('logout') }}" method="POST" class="hidden" x-ref="logoutForm">@csrf</form>
                 </div>
             </div>
         </div>
