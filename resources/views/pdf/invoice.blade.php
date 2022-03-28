6c6c7b35c787470c8511244e2a7afa17d8344e16
Christian Leo-Pernold <christian.leo@akwien.at>
2022-03-28 17:32:04 +0200

    Fix invoice table width.

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -1,118 +1,130 @@
 <!DOCTYPE html>
 <html lang="de">
-    <head>
-        <meta charset="utf-8">
-        <title>Rechnung {{ $invoice->number }}</title>
-        <style type="text/css">
-            * {
-                font-family: Arial, sans-serif;
-                font-size: 1em;
-                color: #040404;
-            }
 
-            body {
-                width: 700px;
-            }
+<head>
+    <meta charset="utf-8">
+    <title>Rechnung {{ $invoice->number }}</title>
+    <style type="text/css">
+        * {
+            font-family: Arial, sans-serif;
+            font-size: 1em;
+            color: #040404;
+        }
 
-            .container {
-                display: inline-block;
-            }
+        body {
+            width: 700px;
+        }
 
-            .header {
-                font-size: 2em;
-                padding-left: 0.5em;
-                padding-top: 0.25em;
-                padding-bottom: 0.25em;
-            }
+        .container {
+            display: inline-block;
+        }
 
-            .main {
-                padding: 0.5em;
-            }
+        .header {
+            font-size: 2em;
+            padding-left: 0.5em;
+            padding-top: 0.25em;
+            padding-bottom: 0.25em;
+        }
 
-            .address {
-                padding-bottom: 0.5em;
-            }
+        .main {
+            padding: 0.5em;
+        }
 
-            div {
-                /*line-height: 1.5em;*/
-            }
+        .address {
+            padding-bottom: 0.5em;
+        }
 
-            h1 {
-                font-size: 1.25em;
-                text-transform: uppercase;
-            }
+        div {
+            /*line-height: 1.5em;*/
+        }
 
-            .small {
-                font-size: 0.5em;
-                line-height: 1.5em
-            }
+        h1 {
+            font-size: 1.25em;
+            text-transform: uppercase;
+        }
 
-            .footer {
-                width: 100%;
-                font-size: 0.75em;
-            }
+        .small {
+            font-size: 0.5em;
+            line-height: 1.5em
+        }
 
-            .has-no-wrap {
-                white-space: nowrap;
-            }
+        .footer {
+            width: 100%;
+            font-size: 0.75em;
+        }
 
-            table, tr, td, th {
-                width: 100%;
-                border-collapse: collapse;
-                padding: 0.45em;
-            }
+        .has-no-wrap {
+            white-space: nowrap;
+        }
 
-            th {
-                text-align: left;
-                background-color: #0099cc;
-                color: #fff;
-            }
+        .w-full {
+            width: 700px;
+        }
 
-            th:not(:last-child) {
-                border-right: 1px dashed #fff;
-            }
-            td:not(:last-child) {
-                border-right: 1px dashed #0099cc;
-            }
-            .is-text-right {
-                text-align: right;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="header">
-            mazedlx.net webproductions<br>
-            <p class="small">DI(FH) Christian Leo-Pernold<br>
+        table,
+        tr,
+        td,
+        th {
+            width: 100%;
+            border-collapse: collapse;
+            padding: 0.45em;
+        }
+
+        th {
+            text-align: left;
+            background-color: #0099cc;
+            color: #fff;
+        }
+
+        th:not(:last-child) {
+            border-right: 1px dashed #fff;
+        }
+
+        td:not(:last-child) {
+            border-right: 1px dashed #0099cc;
+        }
+
+        .is-text-right {
+            text-align: right;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="header">
+        mazedlx.net webproductions<br>
+        <p class="small">DI(FH) Christian Leo-Pernold<br>
             Glockenblumengasse 36/6/2<br>
             1220 Wien<br>
             Österreich<br>
             +43 660 456 3193<br>
             christian@mazedlx.net</p>
-        </div>
-        <div class="container">
-            <div class="main">
-                <h1>Rechnung</h1>
-                <div class="address">
-                    {{ optional($invoice->company)->name }}<br>
-                    {{ optional($invoice->customer)->name }}<br>
-                    {{ $invoice->address }}<br>
-                    {{ $invoice->zip }} {{ $invoice->city }}<br>
-                    {{ $invoice->country }}
-                </div>
-                <br>
-                Datum: {{ $invoice->date->format('d.m.Y') }}<br>
-                Rechnungsnummer: {{ $invoice->number }}<br><br>
-                <table>
-                    <thead>
-                        <tr>
-                            <th class="has-no-wrap">Beschreibung</th>
-                            <th class="has-no-wrap">Menge (h)</th>
-                            <th class="has-no-wrap">&euro;/h</th>
-                            <th class="has-no-wrap">Kosten</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($invoice->lines as $line)
+    </div>
+    <div class="container">
+        <div class="main">
+            <h1>Rechnung</h1>
+            <div class="address">
+                {{ optional($invoice->company)->name }}<br>
+                {{ optional($invoice->customer)->name }}<br>
+                {{ $invoice->address }}<br>
+                {{ $invoice->zip }} {{ $invoice->city }}<br>
+                {{ $invoice->country }}
+            </div>
+            <br>
+            Datum: {{ $invoice->date->format('d.m.Y') }}<br>
+            Rechnungsnummer: {{ $invoice->number }}<br><br>
+            <table class="w-full">
+                <thead>
+                    <tr>
+                        <th class="has-no-wrap">Beschreibung</th>
+                        <th class="has-no-wrap">Menge (h)</th>
+                        <th class="has-no-wrap">&euro;/h</th>
+                        <th class="has-no-wrap">Kosten</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($invoice->lines as $line)
                         <tr>
                             <td>{{ $line->task }}</td>
                             <td class="has-no-wrap">{{ $line->timeInHours }}</td>
@@ -120,28 +132,33 @@
                             <td class="is-text-right has-no-wrap">&euro; {{ $line->amountInEuros }}</td>
                         </tr>
                     @endforeach
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <th colspan="3" class="is-text-right has-no-wrap">Gesamt</th>
-                            <th class="is-text-right has-no-wrap">&euro; {{ $invoice->amountInEuros }}</th>
-                        </tr>
-                    </tfoot>
-                </table>
-                Steuerbefreit – Kleinunternehmer gemäß &sect;6(1)27 UStG<br>
-                Wir danken f&uuml;r Ihren Auftrag!<br>
-                <br>
-                Mit freundlichen Gr&uuml;&szlig;en<br>
-                <br>
-                DI(FH) Christian Leo-Pernold<br>
-                <br>
-                mazedlx.net webproductions<br>
-                <p class="footer">Bitte &uuml;berweisen Sie den Betrag innerhalb von 14 Tagen auf folgendes Konto:<br>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th
+                            colspan="3"
+                            class="is-text-right has-no-wrap"
+                        >Gesamt</th>
+                        <th class="is-text-right has-no-wrap">&euro; {{ $invoice->amountInEuros }}</th>
+                    </tr>
+                </tfoot>
+            </table>
+            Steuerbefreit – Kleinunternehmer gemäß &sect;6(1)27 UStG<br>
+            Wir danken f&uuml;r Ihren Auftrag!<br>
+            <br>
+            Mit freundlichen Gr&uuml;&szlig;en<br>
+            <br>
+            DI(FH) Christian Leo-Pernold<br>
+            <br>
+            mazedlx.net webproductions<br>
+            <p class="footer">Bitte &uuml;berweisen Sie den Betrag innerhalb von 14 Tagen auf folgendes
+                Konto:<br>
                 Inhaber: DI(FH) Christian Leo-Pernold<br>
                 IBAN: AT30 1400 0018 1094 4907<br>
                 BIC: BAWAATWW
-                </p>
-            </div>
+            </p>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
