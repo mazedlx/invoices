3232e795fbbd7053774dc69a86de6fae14b517ca
Christian Leo-Pernold <mazedlx@gmail.com>
2025-03-02 10:16:24 +0100

    WIP.

--- resources/views/livewire/invoices/edit.blade.php
+++ resources/views/livewire/invoices/edit.blade.php
@@ -1,8 +1,19 @@
-<form action="#" method="POST">
+<form
+    action="#"
+    method="POST"
+>
     <div class="shadow-sm sm:rounded-md sm:overflow-hidden">
         <div class="px-4 py-5 space-y-6 bg-white sm:p-6">
-            <x-input for="number" label="Number" model="invoice.number" />
-            <x-date for="date" label="Date" model="invoice.date" />
+            <x-input
+                for="number"
+                label="Number"
+                model="invoice.number"
+            />
+            <x-date
+                for="date"
+                label="Date"
+                model="invoice.date"
+            />
             <x-select
                 model="invoice.customer_id"
                 for="customer_id"
@@ -19,20 +30,48 @@
                 key="id"
                 show="name"
             />
-            <x-input for="address" label="Address" model="invoice.address" />
-            <x-input for="zip" label="Zip" model="invoice.zip" />
-            <x-input for="city" label="City" model="invoice.city" />
-            <x-input for="country" label="Country" model="invoice.country" />
-            <x-checkbox for="paid" value="1" label="Paid?" model="invoice.paid" />
+            <x-input
+                for="address"
+                label="Address"
+                model="invoice.address"
+            />
+            <x-input
+                for="zip"
+                label="Zip"
+                model="invoice.zip"
+            />
+            <x-input
+                for="city"
+                label="City"
+                model="invoice.city"
+            />
+            <x-input
+                for="country"
+                label="Country"
+                model="invoice.country"
+            />
+            <x-checkbox
+                for="paid"
+                value="1"
+                label="Paid?"
+                model="invoice.paid"
+            />
 
             <x-invoice-lines :lines="$lines" />
 
             <div class="flex justify-end mt-2">
                 <div class="pt-5">
                     <div class="flex justify-end">
-                        <a href="{{ route('invoices.show', $invoice) }}" class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-xs hover:bg-gray-50 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Cancel</a>
+                        <a
+                            href="{{ route('invoices.show', $invoice) }}"
+                            class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-xs hover:bg-gray-50 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                        >Cancel</a>
 
-                        <button wire:click.prevent="update" type="submit" class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-xs hover:bg-gray-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">Update</button>
+                        <button
+                            wire:click.prevent="update"
+                            type="button"
+                            class="inline-flex justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-gray-600 border border-transparent rounded-md shadow-xs hover:bg-gray-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                        >Update</button>
                     </div>
                 </div>
             </div>
